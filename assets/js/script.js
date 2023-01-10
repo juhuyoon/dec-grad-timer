cbfe08803502b50f3beb0cf39ae2787528e8e5dc
Junghoon Yoon <jyoon3@2u.com>
2023-01-10 13:54:02 -0500

    got current time to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log("HEWWO?");
+var currentTime = dayjs();
+var timerDisplayEl = document.getElementById("timer-display");
+
+console.log(currentTime.format("MMM D, YYYY"));
+
+function displayCurrentDate() {
+  var formattedDate = currentTime.format("MMM D, YYYY");
+  timerDisplayEl.textContent = formattedDate;
+}
+
+displayCurrentDate();
