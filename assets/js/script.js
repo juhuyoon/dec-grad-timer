86b9c50d8b6cca58830f11627c495171f5ea02df
Junghoon Yoon <jyoon3@2u.com>
2023-01-10 14:04:40 -0500

    got the countdown to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
 var currentTime = dayjs();
 var timerDisplayEl = document.getElementById("timer-display");
+var countdownGradEl = document.querySelector("#countdown-graduation");
 
 console.log(currentTime.format("MMM D, YYYY"));
 
 function displayCurrentDate() {
   var formattedDate = currentTime.format("MMM D, YYYY");
   timerDisplayEl.textContent = formattedDate;
+  calculateGraduationDate();
+}
+
+function calculateGraduationDate() {
+  //   console.log("Inside calculate", currentDate);
+  var gradDate = dayjs("2023-03-16").format("MMM D, YYYY");
+  //   console.log("gradDate", gradDate);
+  //   console.log(gradDate.diff(currentDate));
+  var daysRemaining = dayjs("2023-03-16").diff(dayjs(), "day");
+
+  countdownGradEl.textContent =
+    daysRemaining + " days remaining before J O B U!";
 }
 
 displayCurrentDate();
