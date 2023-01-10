53005fbf806541e10df7c37aafc36d63dfc9211a
Junghoon Yoon <jyoon3@2u.com>
2023-01-10 14:25:56 -0500

    added writing notes and saved it to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,12 @@ var noteDisplayEl = document.getElementById("note-display");
 
 console.log(currentTime.format("MMM D, YYYY"));
 
+// This function will call ALL the functions that should execute WHEN the program starts.
+function init() {
+  displayCurrentDate();
+  displayExistingNotes();
+}
+
 function displayCurrentDate() {
   var formattedDate = currentTime.format("MMM D, YYYY");
   timerDisplayEl.textContent = formattedDate;
@@ -23,7 +29,10 @@ function calculateGraduationDate() {
     daysRemaining + " days remaining before J O B U!";
 }
 
-displayCurrentDate();
+function displayExistingNotes() {
+  var returningData = JSON.parse(localStorage.getItem("studentNotes"));
+  console.log(returningData);
+}
 
 function writeNotes(event) {
   event.preventDefault();
@@ -31,10 +40,31 @@ function writeNotes(event) {
   var notesText = document.getElementById("notesText").value.trim();
   //   textContent seems to OVERRIDE existing text
   //   noteDisplayEl.textContent = notesText;
+
+  // OPTIMIZATIONS/GOOD PRACTICES
+  if (!notesText) {
+    alert("TYPE SOMETHING INTO THE BOX OR ELSE PERISH!");
+    return;
+  }
+  putInLocalStorage(notesText);
+
   var cardDiv = document.createElement("div");
   cardDiv.setAttribute("class", "text-success");
   cardDiv.textContent = notesText;
   noteDisplayEl.appendChild(cardDiv);
 }
 
+function putInLocalStorage(notes) {
+  console.log("inlocalstoragefunction", notes);
+  var storageArray = [];
+  if (notes) {
+    // TODO: Right now, it seems to DELETE the old array data and create a NEW array. So I should FIX THAT
+    storageArray.push(notes);
+    localStorage.setItem("studentNotes", JSON.stringify(storageArray));
+  }
+}
+
 noteBtn.addEventListener("click", writeNotes);
+
+// Call this function to make certain functions run on page-load
+init();
